9186a68748ae0c402ddf8861d6a22059855e8f56
sudip110 <sudiptom172@gmail.com>
2024-04-29 13:57:28 +0530

    fix bug sidebar disappearing after clicking anywhere other than the menuItem in mobile view

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,12 +13,12 @@ function Header() {
         <div class=" top-0 ">
             <nav role="navigation" class=""> 
                {/* Menu button on medium and smaller screens */}
-                <button class={` z-30 lg:hidden  peer ${menuOpen?"-left-96":"left-0"} relative transition delay-150 duration-300` } onClick={handleClickMenu}>
+                <button class={` z-20 lg:hidden  peer  relative transition delay-150 duration-300` } onClick={handleClickMenu}>
                    <MenuIcon/>
                 </button>
 
                 {/* Navigation bar/Sidebar */}
-                <div class="z-20 text-gray-500 lg:p-2 lg:static fixed top-0 -left-96 lg:left-0 lg:h-12 h-screen w-1/4 lg:w-screen bg-ivory-white shadow-2xl lg:shadow-sm peer-focus:left-0 peer:transition ease-out delay-150 duration-500">
+                <div class="z-30 text-gray-500 lg:p-2 lg:static fixed top-0 -left-96 lg:left-0 lg:h-12 h-screen w-1/4 lg:w-screen bg-ivory-white shadow-2xl lg:shadow-sm peer-focus:left-0 peer:transition ease-out delay-150 duration-500">
                    
                     <div class="flex lg:flex-row flex-col lg:items-center justify-center gap-4"> 
                         
@@ -32,19 +32,19 @@ function Header() {
                         </div>
                         
                         {/* Home section */}
-                        <div class="flex  cursor-pointer lg:h-fit h-12 lg:bg-ivory-white bg-whitesmoke shadow-md lg:shadow-none rounded-md lg:rounded-none gap-2 text-base items-center pl-2 lg:hover:text-violet-500" onClick={()=>setMenuOpen(false)}>
+                        <div class="flex  cursor-pointer lg:h-fit h-12 lg:bg-ivory-white bg-whitesmoke shadow-md lg:shadow-none rounded-md lg:rounded-none gap-2 text-base items-center pl-2 lg:hover:text-violet-500">
                             <div  class="lg:hidden"><HomeOutlinedIcon /></div>
                             <button class="lg:focus:text-violet-500  font-medium">Home</button>
                         </div>
 
                         {/* About Section */}
-                        <div class="flex cursor-pointer lg:h-fit h-12 lg:bg-ivory-white bg-whitesmoke  shadow-md lg:shadow-none rounded-md lg:rounded-none gap-2 text-base items-center pl-2 lg:hover:text-violet-500" onClick={()=>setMenuOpen(false)}>
+                        <div class="flex cursor-pointer lg:h-fit h-12 lg:bg-ivory-white bg-whitesmoke  shadow-md lg:shadow-none rounded-md lg:rounded-none gap-2 text-base items-center pl-2 lg:hover:text-violet-500">
                             <div  class="lg:hidden  "><Person2OutlinedIcon/></div>
                             <button class="lg:focus:text-violet-500  font-medium">About</button>
                         </div>
 
                         {/* Project Section */}
-                        <div class="flex cursor-pointer lg:h-fit h-12  lg:bg-ivory-white bg-whitesmoke  shadow-md lg:shadow-none rounded-md lg:rounded-none gap-2 text-base items-center pl-2 lg:hover:text-violet-500" onClick={()=>setMenuOpen(false)}>
+                        <div class="flex cursor-pointer lg:h-fit h-12  lg:bg-ivory-white bg-whitesmoke  shadow-md lg:shadow-none rounded-md lg:rounded-none gap-2 text-base items-center pl-2 lg:hover:text-violet-500" >
                              <div  class="lg:hidden "><AccountTreeOutlinedIcon/></div>
                             <button class="lg:focus:text-violet-500  font-medium">Projects</button></div>
                         </div>
